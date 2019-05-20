adb11d6cff223ae544355c7fedceddc6c3870951
Simon Wicha <simon.wicha@gaiaresources.com.au>
2019-05-20 16:04:23 +0800

    added database extration codeblocks

--- src/app/project-selector/project-selector.page.ts
+++ src/app/project-selector/project-selector.page.ts
@@ -6,6 +6,9 @@ import { from } from 'rxjs';
 import { FilePath } from '@ionic-native/file-path/ngx';
 import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
+import { SqliteDbCopy } from '@ionic-native/sqlite-db-copy/ngx';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import * as Base64 from 'base-64';
 
 
 @Component({
@@ -20,7 +23,8 @@ export class ProjectSelectorPage implements OnInit {
 
   constructor(private loadingCtrl: LoadingController, private alertController: AlertController,
               private router: Router, public mobileState: MobileService,
-              private transfer: FileTransfer, private file: File) {
+              private transfer: FileTransfer, private file: File,
+              private sqliteDbCopy: SqliteDbCopy, private sqlite: SQLite) {
   }
 
   fileTransfer: FileTransferObject = this.transfer.create();
@@ -96,7 +100,11 @@ export class ProjectSelectorPage implements OnInit {
                 console.log('mbtile_url', url);
                 this.fileTransfer.download(url, this.file.documentsDirectory + '/mapdata/' + proj.code + '.mbtile').then((entry) => {
                   console.log('download complete: ' + entry.toURL());
-
+                  // copy mbtile file to platform specific location for extraction
+                  this.sqliteDbCopy.copyDbFromStorage(proj.code + '.mbtiles', 0,  entry.toURL(), true).then((success) => {
+                    console.log('DB copied');
+                    this.extractDataBase(proj.code);
+                  }).catch(e => console.log(e));
                 }, (error) => {
                   console.log('download error ' + error);
                 });
@@ -114,5 +122,109 @@ export class ProjectSelectorPage implements OnInit {
       this.mobileState.offlineToggle();
     }
     return;
+  }
+
+  private extractDataBase(dbName: string) {
+    const sqlpromise = this.sqlite.create({ name: dbName + '.mbtiles', location: 'default', createFromLocation: 0 });
+
+    sqlpromise.then((db: SQLiteObject) => {
+      const mapFileLocation = this.file.documentsDirectory + '/mapdata/' + dbName;
+      this.file.createDir(this.file.documentsDirectory, 'mapdata', true).then(() => {
+        this.file.checkDir(this.file.documentsDirectory + '/mapdata/', dbName).then((exists) => {
+          if (exists == true) {
+            console.log('Tile data already copied - loading map');
+            // Ready to load map
+          }
+        }).catch((e) => {
+          console.log('Tile data required to get copied...' + e);
+          this.file.createDir(this.file.documentsDirectory + 'mapdata/', dbName, true).then(() => {
+            db.executeSql('SELECT count(*) AS dbCount FROM tiles', [])
+            .then((resultSet) => {
+              const dbCount = resultSet.rows.item(0).dbCount;
+              const offsetLimit = Math.ceil(dbCount / 1000);
+              this.presentLoading({
+                message: 'Loading map data...'
+              });
+              for(let i = 0; i <= offsetLimit; i++) {
+                console.log('extractTilesToFileSystem ', i);
+                this.extractTilesToFileSystem(db, i * 1000, dbCount, 1000, mapFileLocation);
+              }
+            });
+          });
+        });
+      });
+    });
+  }
+
+  setLoadingText(text:string) {
+    const elem = document.querySelector(
+      "div.loading-wrapper div.loading-content");
+      if(elem) elem.innerHTML = text;
+  }
+
+  private extractTilesToFileSystem(db: SQLiteObject, cursor: number, cursorSize: number, limitSize: number, mapFileLocation: string) {
+    db.executeSql('SELECT tile_row AS row, tile_column AS column, zoom_level AS zoom, hex(tile_data) AS data FROM tiles ORDER BY zoom_level, tile_row, tile_column LIMIT ' + limitSize +' OFFSET ' + cursor, [])
+    .then((resultSet) => {
+      console.log('__Current Cursor ' + cursor + ' of '+ cursorSize);
+      const progress =  (cursor / cursorSize) * 100;
+      console.log('__Progress: ' + progress + ' % ');
+      this.setLoadingText(parseInt(progress.toString()) + ' % done');
+      const db_data = resultSet.rows;
+      for(let i = 0; i <= db_data.length; i++){
+        const x = db_data.item(i).column;
+        const y = db_data.item(i).row;
+        const zoom = db_data.item(i).zoom;
+        const newY = (1 << zoom) - y - 1;
+        const blob = db_data.item(i).data;
+        let bString = '';
+        for (let i = 0; i < blob.length; i += 2) {
+          bString += String.fromCharCode( parseInt( blob.substr( i, 2), 16));
+        }
+        const imageData = Base64.encode(bString);
+        const base64ImageData = 'data:image/png;base64,' + imageData;
+        const imageBlob = this.b64toBlob(imageData, 'image/png', 512)
+
+        // TODO: iOS specific folder, probably need changing for Android
+        this.file.writeFile(mapFileLocation, zoom + '-' + x + '-' + newY + '.png', imageBlob, {replace: true})
+        .catch((err) => {
+          console.error(err);
+        })
+      }
+    })
+    .finally(() => {
+      if (cursor >= cursorSize) {
+        console.log('Loading Map');
+        if (this.loading) {
+          this.loading.dismiss();
+        }
+        console.log('Map extracted successfully');
+        // Ready to load map
+      }
+    })
+    .catch(e => console.log(e));
+  }
+
+  private b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      var byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
   }
 }
